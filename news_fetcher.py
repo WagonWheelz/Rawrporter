6faf10ebac5b2b30a5af3e5ad2ccd3f088af6d71
WagonWheelz <Jakob.harrison2001@protonmail.com>
2025-06-29 21:30:23 +1000

    Update news_fetcher.py
    - Added the ability to pull thumbnail images

--- news_fetcher.py
+++ news_fetcher.py
@@ -30,9 +30,34 @@ def fetch_news():
             headline = entry.title
             if not contains_blocked_word(headline):
                 link = entry.link
+
+                thumb_url = None
+                if 'media_content' in entry:
+                    biggest = None
+                    biggest_width = -1
+                    for media in entry.media_content:
+                        try:
+                            width = int(media.get('width', 0))
+                        except ValueError:
+                            width = 0
+                        if width > biggest_width:
+                            biggest = media
+                            biggest_width = width
+                    if biggest:
+                        thumb_url = biggest['url']
+                elif 'media_thumbnail' in entry:
+                    thumb_url = entry.media_thumbnail[0]['url']
+                elif 'links' in entry:
+                    for l in entry.links:
+                        if l.get('rel') == 'enclosure' and l.get('type', '').startswith('image'):
+                            thumb_url = l['href']
+                            break
+
                 print("[News Fetcher] Headline:", headline)
                 print("[News Fetcher] Link:", link)
-                articles.append((headline, link))
+                print("[News Fetcher] Thumbnail:", thumb_url)
+
+                articles.append((headline, link, thumb_url))
         if articles:
             return articles
         else:
