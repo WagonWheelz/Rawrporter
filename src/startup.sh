ad08358d9a5b786953bdbdadbf0e4b147258a4f6
tristan <tristandcr@gmail.com>
2025-06-30 16:39:47 +1000

    I really need to learn to spell

--- src/startup.sh
+++ src/startup.sh
@@ -4,7 +4,7 @@ export POSTED_DATE=$time
 echo "Running in Docker: "$DOCKER
 echo "Monitored RSS Feed: "$RSS_FEED
 echo "BSKY Account: "$USERNAME
-echo "BSKY App Passwod: "$APP_PASSWORD
+echo "BSKY App Password: "$APP_PASSWORD
 echo "Starting Time: "$POSTED_DATE
 echo "Executing bot run"
 /usr/bin/python /app/main.py
